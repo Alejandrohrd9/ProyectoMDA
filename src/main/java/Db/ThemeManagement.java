df2078f371b89217c38652003a87f44037e8c284
Nacho <nacho1714@gmail.com>
2018-04-16 10:54:48 +0100

    Scrum manager: correcion error

--- src/main/java/Db/ThemeManagement.java
+++ src/main/java/Db/ThemeManagement.java
@@ -7,9 +7,7 @@ package Db;
 
 import com.mycompany.tutordocs.Message;
 import com.mycompany.tutordocs.Theme;
-import com.sun.javafx.scene.control.skin.VirtualFlow;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
